aa240dca3ee237bcc06a2a67151cdd1c8bd78e04
Jorge Gómez <syvic@sindormir.net>
2024-01-26 10:25:14 +0100

    Update tema34_l.ino

--- tema34_l.ino
+++ tema34_l.ino
@@ -1,5 +1,5 @@
 while (WiFi.status() != WL_CONNECTED) {
-  Serial.print("WiFi desconectado. Intentando reconexión");
+  Serial.println("WiFi desconectado. Intentando reconexión");
   WiFi.disconnect();
   WiFi.reconnect();
   delay(2500);
